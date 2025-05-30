2037e03b7ac86ba85050b720a0469b8196deb50c
Chang Sun <chsun@cern.ch>
2025-05-30 02:01:17 -0700

    quality-of-life changes

--- hls4ml/model/graph.py
+++ hls4ml/model/graph.py
@@ -770,6 +770,12 @@ class ModelGraph:
         return int(n_sample)
 
     def predict(self, x):
+        if isinstance(x, np.ndarray) and not x.flags['C_CONTIGUOUS']:
+            x = np.ascontiguousarray(x)
+
+        # Compile the model if it wasn't compiled yet
+        if self._top_function_lib is None:
+            self.compile()
         top_function, ctype = self._get_top_function(x)
         n_samples = self._compute_n_samples(x)
         n_inputs = len(self.get_input_variables())
@@ -785,10 +791,9 @@ class ModelGraph:
                 inp = [np.asarray(x[i])]
             else:
                 inp = [np.asarray(xj[i]) for xj in x]
-            argtuple = inp
-            argtuple += predictions
-            argtuple = tuple(argtuple)
-            top_function(*argtuple)
+            inp = [_inp if _inp.flags['C_CONTIGUOUS'] else np.ascontiguousarray(_inp) for _inp in inp]
+
+            top_function(*inp, *predictions)
             output.append(predictions)
 
         # Convert to list of numpy arrays (one for each output)
